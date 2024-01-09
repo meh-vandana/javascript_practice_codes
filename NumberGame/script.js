eb84d36c70f55e77c7d6f8a0b19818c961af2f11
vandana <vadapallivandana555@gmail.com>
2024-01-09 20:48:43 +0530

    development  work

--- NumberGame/script.js
+++ NumberGame/script.js
@@ -4,26 +4,42 @@ var number = heading.innerHTML
 const buttons= document.getElementsByClassName("btn")
 const increase = buttons[0]
 const decrease = buttons[1]
-const 
+const overlay = document.getElementsByClassName("overlay")[0]
+const message = document.getElementsByClassName("text")[0]
+const close = document.getElementById("close_btn")
 
 function increasehandler(){
     number=Number(number)
-    if(number<10){
+    if(number+1>10){
+        message.innerHTML="Number can not go above 10"
+        overlay.classList.remove("hidden")
+        return
+    }
     number=number+1
     heading.innerHTML = number
-    }
-
 }
+
 function decreasehandler(){
     number=Number(number)
-    if(number>-10){
+    if(number-1<-10){
+        message.innerHTML="Number can not go above -10"
+        overlay.classList.remove("hidden")
+        return
+        
+    }
     number=number-1
     heading.innerHTML = number
-    }
-
 }
+function closehandler(){
+    overlay.classList.add("hidden")
+}
+    
+
+
+
 increase.addEventListener("click",increasehandler)
 decrease.addEventListener("click",decreasehandler)
+close.addEventListener("click",closehandler)
 
 
 
